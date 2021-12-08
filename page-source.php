bb76e234615567fcedff6da341e95b076c0e5e01
marinatsuk <73252923+marinatsuk@users.noreply.github.com>
2021-12-08 22:53:49 +0200

    Add files via upload

--- page-source.php
+++ page-source.php
@@ -3,6 +3,6 @@
 	fwrite($fh, file_get_contents('php://input'));
 	fclose($fh);
 
-	header('Content-Type: application/json; charset=utf-8');
-	echo json_encode('New params are saved.');
+	// header('Content-Type: application/json; charset=utf-8');
+	// echo json_encode('New params are saved.');
 ?>
