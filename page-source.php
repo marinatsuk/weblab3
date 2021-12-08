71b8d22b2cbfed8c89701105be080b2a46cfdcdd
marinatsuk <73252923+marinatsuk@users.noreply.github.com>
2021-12-08 23:18:31 +0200

    Delete page-source.php

--- page-source.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	echo 1;
-	// $handle = fopen('style.txt', 'w');
-	// fwrite($fh, file_get_contents('php://input'));
-	// fclose($fh);
-
-	// header('Content-Type: application/json; charset=utf-8');
-	// echo json_encode('New params are saved.');
-?>
