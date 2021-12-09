173ac3b597b424d5522b6611761ed40e42f15088
marinatsuk <73252923+marinatsuk@users.noreply.github.com>
2021-12-09 19:59:27 +0200

    Add files via upload

--- page-source.php
+++ page-source.php
@@ -1,14 +1,15 @@
 <?php 
-	try {
-		$file_handle = fopen('style.json', 'w');
-		fwrite($file_handle, json_encode($_POST));
-		fclose($file_handle);
+	// try {
+	// 	$file_handle = fopen('style.json', 'w');
+	// 	fwrite($file_handle, json_encode($_POST));
+	// 	fclose($file_handle);
 
-		$response['status'] = 'success';
-	} catch (Exception $e) {
-		$response = array('error' => $e->getMessage());
-	}
-
+	// 	$response['status'] = 'success';
+	// } catch (Exception $e) {
+	// 	$response = array('error' => $e->getMessage());
+	// }
+	
+	$response['status'] = 'success';
 	$response = json_encode($response);
 	header('Access-Control-Allow-Origin: *');
 	header('Content-type: application/json; charset=utf-8');
