7be0535b843df3c26c377b87f84059e4b0eb3f19
marinatsuk <73252923+marinatsuk@users.noreply.github.com>
2021-12-09 08:43:34 +0200

    Add files via upload

--- page-source.js
+++ page-source.js
@@ -2,8 +2,8 @@ $("#inputter").submit(function(e){
 	alert("!");
 	e.preventDefault();
 
-	$.post("page-source.py", $(this).serialize(), function(result) {
-		alert( "success" );
+	$.post("page-source.php", $(this).serialize(), function(result) {
+		alert(result);
 	})
 	.done(function() {
 		alert( "second success" );
